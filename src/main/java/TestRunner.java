3152ebbb5eb54e3ba6be879bec1400a02de84346
sdmnc <sergedominik@gmail.com>
2023-04-26 17:51:28 +0300

    Practise lesson 15 main

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -1,2 +1,14 @@
+import Managers.WebDriverManager;
+
 public class TestRunner {
+
+    public static void main (String [] args) throws InterruptedException {
+
+        WebDriverManager webDriverManager = new WebDriverManager("CHROME");
+        webDriverManager.getDriver().get("https://demo.opencart.com/admin/");
+        Thread.sleep(10000);
+        webDriverManager.closeDriver();
+
+
+    } // end main
 }
